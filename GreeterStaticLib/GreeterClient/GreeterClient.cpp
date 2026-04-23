093789b5a794fe705349ea0491143113c72af699
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-04-23 14:24:40 +0300

    Add files via upload

---
+++ GreeterStaticLib/GreeterClient/GreeterClient.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include "Greeter.h"
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::getline(std::cin, name);
+    Greeter greeter;
+    std::cout << greeter.greet(name) << std::endl;
+
+    return 0;
+}
